507bec0d0250c923dba7bbaa1cbb2f579de35f61
lucas costa <lucaszcosta@hotmail.com.br>
2024-03-08 15:16:09 -0300

    partial class da program no arquivo filestream

---
+++ ByteBankIO/FileStream.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+partial class Program
+{
+    static void FileStream()
+    {
+        string nomeArquivo = "contas.txt";
+        var numeroDeBytesLidos = -1;
+
+
+        using (var fluxoDoArquivo = new FileStream(nomeArquivo, FileMode.Open))
+        {
+            var buffer = new byte[1024]; //1kb
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, buffer.Length);
+                Console.WriteLine($"\nBites lidos: {numeroDeBytesLidos}");
+
+                EscreverBuffer(buffer, numeroDeBytesLidos);
+            }
+
+            fluxoDoArquivo.Close();
+
+        };
+
+
+
+
+        Console.ReadLine();
+    }
+
+    static void EscreverBuffer(byte[] buffer, int bytesLidos)
+    {
+
+        var utf8 = new UTF8Encoding();
+        var text = utf8.GetString(buffer, 0, bytesLidos);
+
+        Console.Write(text);
+
+
+    }
+
+}
--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -4,38 +4,8 @@ class Program
 {
     static void Main()
     {
-        string nomeArquivo = "contas.txt";
-        var numeroDeBytesLidos = -1;
-
-
-        var fluxoDoArquivo = new FileStream(nomeArquivo, FileMode.Open);
-        var buffer = new byte[1024]; //1kb
-
-
-        
-        while(numeroDeBytesLidos != 0)
-        {
-            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-            EscreverBuffer(buffer);
-        }
-
-
-        
-        
         Console.ReadLine();
     }
 
-    static void EscreverBuffer(byte[] buffer)
-    {
-
-        var utf8 = new UTF8Encoding();
-        var text = utf8.GetString(buffer);
-        Console.WriteLine(text);
-        /*foreach (var meuByte in buffer)
-        {
-            Console.Write(meuByte);
-            Console.Write(" ");
-        }*/
-    }
 
 }
