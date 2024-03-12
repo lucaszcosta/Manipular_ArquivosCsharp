fb1e439a8771feabe0bf88d8fa9e0ad0103f4d2a
lucas costa <lucaszcosta@hotmail.com.br>
2024-03-12 15:55:25 -0300

    Aplicando a conversão da string recebida no arquivo para um tipo ContaCorrente

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using ByteBankIO;
+using System.Text;
 
 partial class Program
 {
@@ -16,13 +17,42 @@ partial class Program
             while(!leitor.EndOfStream)
             {
                 var linha = leitor.ReadLine();
-                Console.WriteLine(linha);
+                var contaCorrente = ConverterStringParaContaCorrente(linha);
+
+                var msg = $"{contaCorrente.Titular.Nome}Conta numero: {contaCorrente.Numero}, Agencia: {contaCorrente.Agencia}, Saldo: {contaCorrente.Saldo}";
+                Console.WriteLine(msg);
             }
 
         }
 
 
         Console.ReadLine();
+    }
+    static ContaCorrente ConverterStringParaContaCorrente(string linha)
+    {
+
+
+        var campos = linha.Split(',');
+
+        var agencia = campos[0];
+        var numero = campos[1];
+        var saldo = campos[2];
+        var nomeTitular = campos[3];
+        
+        
+        var agenciaComInt = int.Parse(agencia);
+        var numeroComInt = int.Parse(numero);
+        var saldoComDouble = double.Parse(saldo);
+
+        var titular = new Cliente();
+        titular.Nome = nomeTitular;
+
+
+        var resultado = new ContaCorrente(agenciaComInt, numeroComInt);
+        resultado.Depositar(saldoComDouble);
+        resultado.Titular = titular;
+
+        return resultado;
     }
 
 
